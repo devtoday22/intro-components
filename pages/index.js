084e32f05b21b64f199b95e4308296be558b7ed8
Levan Jgerenaia <devtoday22@gmail.com>
2024-11-11 22:23:25 +0400

    added new favicon and deleted default images

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
      {/* Body part */}
+     <h1>wefnewjfn fwfnwen fwjfw</h1>
+     <h1>wefnewjfn fwfnwen fwjfw</h1>
+     
     </>
   );
 }
